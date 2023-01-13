5bf257717f082cdab12fa9c67561f4609a2abaf0
Carlos Mertens <mertenscarlos@yahoo.com>
2023-01-13 22:37:30 +0100

    style: add Lato font family

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Lato } from '@next/font/google';
+
+const lato = Lato({
+  weight: ['400', '900'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={lato.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
