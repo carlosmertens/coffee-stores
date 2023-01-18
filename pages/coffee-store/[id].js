befa4660e7500ab3c9a0c4ad8cf17a28e3371d76
Carlos Mertens <mertenscarlos@yahoo.com>
2023-01-18 14:31:01 +0100

    feat: create getStaticProps and getStaticPaths for dynamic id page

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,21 +1,47 @@
 import Head from 'next/head';
 import Link from 'next/link';
+import { stringify } from 'querystring';
 import { useRouter } from 'next/router';
+import coffeeStoresData from '../../data/coffee-stores.json';
 
-export default function CoffeeStore() {
+export async function getStaticProps({ params }) {
+  return {
+    props: {
+      coffeeStore: coffeeStoresData.find((store) => {
+        return store.id.toString() === params.id; //dynamic id
+      }),
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [
+      { params: { id: '0' } },
+      { params: { id: '1' } },
+      { params: { id: '300' } },
+    ],
+    fallback: true,
+  };
+}
+
+export default function CoffeeStore({ coffeeStore }) {
   const router = useRouter();
+
+  if (router.isFallback) return <div>Loading...</div>;
+
   return (
     <>
       <Head>
-        <title>{router.query.id}</title>
+        <title>{coffeeStore.name}</title>
         <meta
           name='description'
-          content='My favorite coffee shop in the area'
+          content={`${coffeeStore.name} is your favorite coffee shop ${coffeeStore.neighbourhood}`}
         />
       </Head>
-      <h1>{router.query.id}</h1>
-      <p>ID: {router.query.id}</p>
-      <p>My favorite place in Berlin</p>
+      <h1>{coffeeStore.name}</h1>
+      <p>{coffeeStore.neighbourhood}</p>
+      <br />
       <Link href='/'>Back to home</Link>
     </>
   );
